586cd47449772c8f670ee49f6a01ca706605b4df
tadashi622 <62418207+tadashi622@users.noreply.github.com>
2020-04-03 20:55:36 +0900

    ueni

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+server '3.20.161.142', user: 'ec2-user', roles: %w{app db web}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -60,4 +62,3 @@
 #     # password: "please use keys"
 #   }
 
-server '3.20.161.142', user: 'ec2-user', roles: %w{app db web}
