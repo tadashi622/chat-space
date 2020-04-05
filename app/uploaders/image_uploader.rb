eec5d4484749d7b965714364f2a3ce3f35586300
tadashi622 <62418207+tadashi622@users.noreply.github.com>
2020-04-05 16:44:20 +0900

    deploy

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,8 +1,10 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  storage :file
+ 
   include CarrierWave::MiniMagick
   process resize_to_fit: [800, 800]
   
+  storage :fog
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
